633f7481c115c1f06b870031378d2eb7c9e3d6dd
21jun <21jun7654@gmail.com>
2019-12-26 15:54:41 +0900

    add /book/isbn

--- sejong/route.py
+++ sejong/route.py
@@ -2,6 +2,7 @@
 from pathlib import Path
 from flask import request, Response
 from sejong import app
+import requests
 import json
 import datetime
 
@@ -15,3 +16,27 @@ def myconverter(o):
 def main():
     return "hello world 2"
 
+@app.route("/book/isbn", methods=['GET'])
+def get_book_info(isbn):
+
+    response = {
+        'success': True,
+        'data' : {},
+    }
+
+
+    if request.method == 'GET':
+        data = request
+        isbn = data.args.get('isbn')
+
+        url = "https://openapi.naver.com/v1/search/book_adv"
+        params = {'d_isbn': isbn, 'display': '100'}
+        header = {'X-Naver-Client-Id': 'Lv5kVN8rjERQA76WMpen',
+                'X-Naver-Client-Secret': 'EloLcTPux5'}
+        book_response = requests.get(url, params=params, headers=header)
+        data = json.loads(book_response.text)
+        item = data['items'][0]
+
+        response['data'] = item
+        response['success'] = True
+        return Response(json.dumps(response, indent=4, default=myconverter), mimetype='application/json')
